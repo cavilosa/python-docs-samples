c832aa02feed7e34686f64066cc35c397ddd9aef
cavilosa <cavilosa33@gmail.com>
2021-02-18 08:59:13 -0700

    Arts added to the form with some styles

--- appengine/standard/ascii_chan/templates.py
+++ appengine/standard/ascii_chan/templates.py
@@ -19,9 +19,33 @@ class Handler(webapp2.RequestHandler):
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
+class Art(db.Model): #create and entity
+    title = db.StringProperty(required = True)
+    art = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
 class MainPage(Handler):
+    def render_front(self, title="", art="", error ="" ):
+        arts = db.GqlQuery("SELECT * FROM Art "
+                           "ORDER BY created DESC")
+
+        self.render("front.html", title = title, art = art, error = error, arts = arts)
+
     def get(self):
-        self.render("front.html")
+        self.render_front()
+
+    def post(self):
+        title = self.request.get("title")
+        art = self.request.get("art")
+
+        if title and art:
+            a = Art(title=title, art=art)
+            a.put() # store the new art object in the database
+
+            self.redirect("/")
+        else:
+            error = "we need both a title and some artwork!"
+            self.render_front (title, art, error)
 
 app = webapp2.WSGIApplication( [("/", MainPage)
                                 ], debug = True)
