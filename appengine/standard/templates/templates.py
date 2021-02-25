db44e52697fc58b969ce68df458410c7472f2f44
cavilosa <cavilosa33@gmail.com>
2021-02-25 16:07:33 -0700

    Handler updated with more functions

--- appengine/standard/templates/templates.py
+++ appengine/standard/templates/templates.py
@@ -18,6 +18,13 @@ jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
 def escape_html(s):
     return cgi.escape(s, quote = True)
 
+def make_secure_val(val):
+    return "%s|%s" % (val, hmac.new(secret, val).hexdigest())
+
+def check_secure_val(secure_val):
+    val = secure_val.split("|")[0]
+    if secure_val == make_secure_val(val):
+        return val
 
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
@@ -30,8 +37,20 @@ class Handler(webapp2.RequestHandler):
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
+    def set_secure_cookie(self, name, val):
+        cookie_val = make_secure_val(val)
+        self.response.headers.add_header(
+            "Set-Cookie", "%s=%s; Path=/" % (name, cookie_val))
 
+    def read_secure_cookie(self, name):
+        cookie_val = self.request.cookies.get(name)
+        return cookie_val and check_secure_val(cookie_val)
 
+    def initialize(self, *a, **kw):
+        webapp2.RequestHandler.initialize(self, *a, **kw)
+        uid = self.read_secure_cookie("user_id")
+        # reads the cookie, makes sure it is valid and sets user on the handler
+        self.user = uid and User.by_id(int(uid))
 
 class MainPage(Handler):
     def get(self):
